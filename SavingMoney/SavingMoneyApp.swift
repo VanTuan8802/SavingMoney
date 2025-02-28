2a64f9e41aea9237159e75169f1e63ea74d11290
VanTuan8802 <vantuan8802@gmail.com>
2025-02-28 22:17:13 +0700

    Initial commit

---
+++ SavingMoney/SavingMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingMoneyApp.swift
+//  SavingMoney
+//
+//  Created by Tuấn Nguyễn on 28/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SavingMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
